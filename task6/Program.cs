235094123cc101f32f7ec4e2dada8aeba009f72d
Valentin <valkja13@gmail.com>
2023-02-02 19:21:45 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает
+// число и выдает, является ли число четным (делится ли оно на
+// два без остатка).
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+if(number % 2 == 0)
+{
+    Console.Write("Even number");
+}
+else
+{
+    Console.Write("Odd number");
+}
